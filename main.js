ba73be5d89785b224bb47b611f2c104019565d90
Lucas Vitareli <lucas.vitareli@hotmail.com>
2024-06-05 18:26:19 +0100

    commit inicial

---
+++ main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    // Quando o botão dentro do header for clicado, o contúdo dentro da tag 'form' escorrega para baixo
+    $('header button').click(function() {
+        $('form').slideDown();
+    }) 
+
+    // Quando clicar no botao 'Cancelar', o conteúdo dentro da tag 'form' sobe de volta
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp()
+    })
+
+    // Quando o formulário for submetido, previne a ação padrão (envio do formulário)
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const novaImagemURL = $('#endereco-url').val()
+        const novoItem =$('<li style="display: none"></li>')
+        $(`<img src="${novaImagemURL}" />`).appendTo(novoItem)
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${novaImagemURL}" target="_blank" tittle="Ver imagem em tamanho real>
+                Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem)
+        $(novoItem).appendTo('ul')
+        $(novoItem).fadeIn(1000)
+        $('#endereco-url').val('')
+    })
+})
+
+
+
